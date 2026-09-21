ad7ceee9ed97cf98c06da78f54c3d1602d10d545
Denis Makhmudov <tamr13ltzr@gmail.com>
2026-09-21 16:05:09 +0300

    l2 done

--- l2/t14/main.go
+++ l2/t14/main.go
@@ -4,13 +4,23 @@ package main
 import "fmt"
 
 func level1() {
+	defer func() {
+		if r := recover(); r != nil {
+			fmt.Printf("паника обработана на уровне level1: %v", r)
+		}
+	}()
 	level2()
 }
 
 func level2() {
+	defer fmt.Println("завершаем level2")
 	level3()
 }
 
 func level3() {
 	panic("паника в level3")
+}
+
+func main() {
+	level1()
 }
